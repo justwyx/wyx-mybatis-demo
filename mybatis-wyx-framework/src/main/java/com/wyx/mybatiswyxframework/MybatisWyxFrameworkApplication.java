0010e56c80691eecd6a26f448c2d47eb0b21333e
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-07-25 14:36:48 +0800

    add

---
+++ mybatis-wyx-framework/src/main/java/com/wyx/mybatiswyxframework/MybatisWyxFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.wyx.mybatiswyxframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisWyxFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisWyxFrameworkApplication.class, args);
+	}
+
+}
